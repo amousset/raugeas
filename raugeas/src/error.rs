353a1eb835acae94de010e82f1849d5a413a97bc
Alexis Mousset <alexis.mousset@rudder.io>
2024-12-15 15:02:21 +0100

    Implement std::Error for augeas::Error

--- raugeas/src/error.rs
+++ raugeas/src/error.rs
@@ -18,6 +18,8 @@ pub enum Error {
     IO(io::Error),
 }
 
+impl std::error::Error for Error {}
+
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match *self {
